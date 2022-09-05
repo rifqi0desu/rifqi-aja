113f46a13652dbae514f91263e427baca918069b
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-09-05 08:44:45 +0700

    Add files via upload

---
+++ pythone5 - TKJ4.py
@@ -0,0 +1,20 @@
+# dollar ke rupiah dollar * rupiah
+
+dollar = 2
+rupiah = 14.895
+idr = dollar * rupiah
+print("dollar jika di konversikan ke rupiah sebesar", idr)
+
+# uero ke yen uero * yen
+
+uero = 11
+yen = 139.27
+jpy = uero * yen
+print("uero jika di konversikan ke yen sebesar", jpy)
+
+# won ke rupiah won * rupiah
+
+won = 22
+rupiah_2 = 10.91
+idr_2 = won * rupiah
+print("won jika di konversikan ke rupiah sebesar", idr_2)
