7e8884aeb69cdbcd924636e0bd8ee4752cc4db6c
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-09-05 09:56:31 +0700

    Add files via upload

---
+++ python6-tkj4.py
@@ -0,0 +1,21 @@
+# list kosong
+list_kosong = []
+
+# list yang berisi kumpulan string
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+# list yang berisi kumpulan integer
+list_nilai = [80, 70, 90, 60]
+
+# list campuran berbagai tipe data
+list_jawaban = [150, 33.33, 'Presiden Sukarno', False]
+
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
+
+list_kosong: []
+list_buah: ['Pisang', 'Nanas', 'Melon', 'Durian']
+list_nilai: [80, 70, 90, 60]
+list_jawaban: [150, 33.33, 'Presiden Sukarno', False]
