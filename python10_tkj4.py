3d657000eb48c0cea8bde64d688a14c3df316ef8
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-10-10 09:44:33 +0700

    Add files via upload

---
+++ python10_tkj4.py
@@ -0,0 +1,24 @@
+#membuat tipe data koleksi
+
+koleksi_data_str = ["pisang","mangga","jambu","semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["reza kecap", 100, True, "reza kecap"]
+
+print("koleksi data string:",koleksi_data_str)
+
+print("koleksi data integer:",koleksi_data_int)
+
+print("koleksi data cadmpuran:",koleksi_data_mix)
+
+nama_nama_hewan = ("semut, babi, anjing,")
+
+list_nilai_uts = (56,92,80,78)
+nama_teman_sebangku = ("pajri , ilal aseli, bandar apran, angga UB")
+
+print("nama nama hewan:", nama_nama_hewan)
+
+print("nilai uts saya", list_nilai_uts)
+
+print("nama teman sebangku", nama_teman_sebangku)
