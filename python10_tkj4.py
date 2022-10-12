d86fcd5b8a2192768061bac62971fe8590dfa8cb
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-10-12 14:06:08 +0700

    Add files via upload

--- python10_tkj4.py
+++ python10_tkj4.py
@@ -12,13 +12,44 @@ print("koleksi data integer:",koleksi_data_int)
 
 print("koleksi data cadmpuran:",koleksi_data_mix)
 
-nama_nama_hewan = ("semut, babi, anjing,")
+nama_nama_hewan = ["semut","babi", "anjing"]
 
-list_nilai_uts = (56,92,80,78)
-nama_teman_sebangku = ("pajri , ilal aseli, bandar apran, angga UB")
+list_nilai_uts = [56,92,80,78]
+
+nama_teman_sebangku = ["pajri" , "ilal aseli", "bandar apran", "angga UB"]
 
 print("nama nama hewan:", nama_nama_hewan)
 
 print("nilai uts saya", list_nilai_uts)
 
 print("nama teman sebangku", nama_teman_sebangku)
+
+# tampilkan data koleksi dengan indeks
+
+# data ke 2 nama hewan 
+
+print("nama hewan data ke-2:", nama_nama_hewan[1])
+
+# data pertama nilai uts
+
+print("data pertama nilai uts:", list_nilai_uts[0])
+
+# data terakhir nama teman sebangku
+
+print("data terakhir nama teman sebangku:", nama_teman_sebangku[3])
+
+# tambahkan 1 data disetiap data koleksi
+
+nama_nama_hewan.append("asep")
+list_nilai_uts.append(100)
+nama_teman_sebangku.append("sucipto")
+
+# ubahlah data terakhir nilai
+
+list_nilai_uts[-1] = 50
+print(list_nilai_uts)
+
+# ubahlah data ke-3 nama hewan
+
+nama_nama_hewan[2] = "shiroko"
+print(nama_nama_hewan)
