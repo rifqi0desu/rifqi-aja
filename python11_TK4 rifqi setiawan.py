26eda93a0defa0e6b36b2c713e5686b22f582ff4
rifqi0desu <112526454+rifqi0desu@users.noreply.github.com>
2022-10-18 09:45:01 +0700

    Add files via upload

---
+++ python11_TK4 rifqi setiawan.py
@@ -0,0 +1,20 @@
+gender = ("laki laki", "perempuan", "laki laki", "perempuan")
+
+tuple_name =tuple(["jayabaya","Rusdi","Icikiwir","Ivan"])
+
+tuple_tunggal = ("aselole")
+
+
+gabungan = gender + tuple_name
+
+print(gabungan)
+print(tuple_tunggal)
+
+# buat 3 koleksi tuple
+teman =("apran hololaip", "angga tambal ban", "ilal aseli", "pajri predator")
+gender =("netral", "laki laki", "laki laki", "netral")
+gim =("ajurlen", "arsip biru", "genshin infek", "mobail lejen")
+
+print(teman)
+print(gender)
+print(gim)
